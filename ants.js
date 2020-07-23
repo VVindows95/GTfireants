fb0b3383a1081f5255623f82c25e8c57e1a0dc83
VVindows95 <64852747+VVindows95@users.noreply.github.com>
2020-07-23 14:58:42 -0400

    Add files via upload

--- ants.js
+++ ants.js
@@ -1,9 +1,5 @@
 var width = 800;
 let height = 400;
-visualRange = 75;
-centeringFactor = 0.005;
-avoidFactor = 0.05;
-matchingFactor = 0.05;
 
 numAnts = 1;
 margin = 100; 
@@ -11,19 +7,8 @@ turnFactor = 1;
 speedLimit = 15;
 minDistance = 20; 
 
-var fps = 1000;
-
-var fpsInterval, startTime, now, then, elapsed;
 
 var ants = [];
-
-function sizeCanvas() {
-  const canvas = document.getElementById("ants");
-  width = window.innerWidth;
-  height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
-}
 
 function randn_bm()
 {
@@ -48,44 +33,10 @@ function initAnts() {
   }
 }
 
-function animationLoop() // Animation Function
-{
-  requestAnimationFrame(animate);
-
-  now = Date.now();
-  elapsed = now - then;
-
-  if (elapsed > fpsInterval)
-  {
-    then = now - (elapsed % fpsInterval);
-  }
-}
-
 window.onload = () => { // Initialization Function
-  
-  fpsInterval = 100000 / fps;
-  then = Date.now();
-  startTime = then;
-  animationLoop();
+  initAnts();
+  window.requestAnimationFrame(animationLoop);
 };
-
-// Measuring distance from bot to bot 
-function distance(ant1, ant2) {
-  return Math.sqrt(
-    (ant1.x - ant2.x) * (ant1.x - ant2.x) +
-      (ant1.y - ant2.y) * (ant1.y - ant2.y),
-  );
-}
-
-// Finding closest bots 
-function nClosestAnts(ant, n) {
-  // Make a copy
-  const sorted = ants.slice();
-  // Sort the copy by distance from `ant`
-  sorted.sort((a, b) => distance(ant, a) - distance(ant, b));
-  // Return the `n` closest
-  return sorted.slice(1, n + 1);
-}
 
 function keepWithinBounds(ant) {
 
@@ -143,11 +94,6 @@ function animationLoop() {
   // Schedule the next frame
   window.requestAnimationFrame(animationLoop);
 }
-
-window.onload = () => {
-  initAnts();
-  window.requestAnimationFrame(animationLoop);
-};
 
 document.getElementById("reset").onclick = function(){
   console.log("Reset Clicked");
